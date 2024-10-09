01f48e7c12597b3589f96642e3206afd84dffd16
Pablo Fernandez <pfernandez@dataahead.de>
2024-10-09 20:58:17 +0200

    Install Mui dependencies. Initialise AnimalSearch component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import AnimalSearch from "./components/AnimalSearch";
+
 function App() {
   return (
-    <>
-      <h1>Wildlife Ranker</h1>
-    </>
+    <div className="bg-gray-100 h-full w-full flex flex-col">
+      <header>
+        <h1 className="text-center">Wildlife Ranker</h1>
+      </header>
+      <main className="grow flex items-center justify-center">
+        <AnimalSearch></AnimalSearch>
+      </main>
+    </div>
   );
 }
 
